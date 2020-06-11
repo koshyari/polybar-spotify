8347ac947207d7ac2130f94118cdae721761af3e
koshyari <koshyaria@gmail.com>
2020-06-12 04:24:02 +0530

    ignore

--- scripts/spotify.sh
+++ scripts/spotify.sh
@@ -5,7 +5,6 @@ zscroll -l 30 \
         --match-command "playerctl --player=spotify status" \
         --match-text "Playing" "--scroll true" \
         --match-text "Paused" "--scroll false" \
-	--match-text "No players found" "--scroll 0" \
         --update-check true '/home/anshul/.config/polybar/scripts/spotify_status.sh' &
 
 wait
